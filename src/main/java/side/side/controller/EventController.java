0482e4d92e36fb57d51da0ea80f7c25d959214e6
NAME <email adress>
2024-08-04 18:18:37 +0900

    8/4 모든 카테고리 불러오기 foodeventDetail 불러오기 성공

--- src/main/java/side/side/controller/EventController.java
+++ src/main/java/side/side/controller/EventController.java
@@ -32,6 +32,15 @@ public class EventController {
     @Autowired
     private TravelCourseService travelCourseService;
 
+    @Autowired
+    private CulturalFacilityService culturalFacilityService;
+
+    @Autowired
+    private ShoppingEventService shoppingEventService;
+
+    @Autowired
+    private FoodEventService foodEventService;
+
     @GetMapping("/fetchGyeonggi")
     public String fetchGyeonggiEvents() {
         eventService.fetchAndSaveGyeonggiEvents();
@@ -52,7 +61,8 @@ public class EventController {
     ) {
         return eventService.searchEvents(date, region, category);
     }
-    //국문관광정보 행사 데이터
+
+    //국문관광정보 축제/공연/행사 카테고리 데이터
     @GetMapping("/fetch")
     public ResponseEntity<?> fetchEvents(
             @RequestParam String serviceKey,
@@ -63,7 +73,8 @@ public class EventController {
         List<TourEvent> events = eventService.fetchAndSaveEvents(serviceKey, numOfRows, pageNo, eventStartDate);
         return ResponseEntity.ok(events);
     }
-    //국문관광정보 지역 데이터
+
+    //국문관광정보 지역(숙박) 카테고리 데이터
     @GetMapping("/fetchAndSave")
     public ResponseEntity<List<LocalEvent>> fetchAndSaveEventsLocal(
             @RequestParam String numOfRows,
@@ -71,7 +82,8 @@ public class EventController {
         List<LocalEvent> events = localEventService.fetchAndSaveEventsLocal(numOfRows, pageNo);
         return ResponseEntity.ok(events);
     }
-    // 관광지 데이터 불러오기
+
+    // 관광지 카테고리 데이터 불러오기
     @GetMapping("/fetchAndSaveTouristAttractions")
     public ResponseEntity<List<TouristAttraction>> fetchAndSaveTouristAttractions(
             @RequestParam String numOfRows,
@@ -81,7 +93,7 @@ public class EventController {
         return ResponseEntity.ok(attractions);
     }
 
-    // 레포츠 데이터 불러오기
+    // 레포츠 카테고리 데이터 불러오기
     @GetMapping("/fetchAndSaveLeisureSports")
     public ResponseEntity<List<?>> fetchAndSaveLeisureSports(
             @RequestParam String numOfRows,
@@ -90,7 +102,7 @@ public class EventController {
         return ResponseEntity.ok(events);
     }
 
-    // 여행 코스 데이터 불러오기
+    // 여행 코스 카테고리 데이터 불러오기
     @GetMapping("/fetchAndSaveTravel")
     public ResponseEntity<List<TravelCourse>> fetchAndSaveTravelCourses(
             @RequestParam String numOfRows,
@@ -98,5 +110,34 @@ public class EventController {
 
         List<TravelCourse> courses = travelCourseService.fetchAndSaveTravelCourses(numOfRows, pageNo);
         return ResponseEntity.ok(courses);
+    }
+
+    //문화 시설 카테고리 데이터 불러오기
+    @GetMapping("/fetchAndSaveCulturalFacility")
+    public ResponseEntity<List<CulturalFacility>> fetchAndSaveCulturalFacilities(
+            @RequestParam String numOfRows,
+            @RequestParam String pageNo) {
+
+        List<CulturalFacility> facilities = culturalFacilityService.fetchAndSaveCulturalFacilities(numOfRows, pageNo);
+        return ResponseEntity.ok(facilities);
+    }
+
+    //쇼핑 카테고리 데이터 불러오기
+    @GetMapping("/fetchAndSaveShopping")
+    public ResponseEntity<List<ShoppingEvent>> fetchAndSaveShoppingEvents(
+            @RequestParam String numOfRows,
+            @RequestParam String pageNo) {
+
+        List<ShoppingEvent> shopping = shoppingEventService.fetchAndSaveShoppingEvents(numOfRows, pageNo);
+        return ResponseEntity.ok(shopping);
+    }
+    // 음식 카테고리 데이터 불러오기
+    @GetMapping("/fetchAndSaveFood")
+    public ResponseEntity<List<FoodEvent>> fetchAndSaveFoodEvents(
+            @RequestParam String numOfRows,
+            @RequestParam String pageNo) {
+
+        List<FoodEvent> food = foodEventService.fetchAndSaveFoodEvents(numOfRows,pageNo);
+        return ResponseEntity.ok(food);
     }
 }
---
+++ src/main/java/side/side/model/FoodEvent.java
@@ -0,0 +1,31 @@
+package side.side.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class FoodEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String addr1;
+    private String firstimage;
+    private String mapx;
+    private String mapy;
+    private String contentid;
+    private String contenttypeid;
+    private String areacode;
+    private String sigungucode;
+    private String tel;
+    private String overview;
+
+}
